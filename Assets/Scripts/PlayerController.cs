23eb132ed49dac013b6293268e79f8da710fa5ae
Sievers <SievJak26@issaquah.wednet.edu>
2026-02-23 10:40:37 -0800

    touching dodge mechanics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,7 +9,9 @@ public class PlayerController : MonoBehaviour
 
     private bool onGround;
     private float jumps = 0;
+    private float fastFallSpeed = -20;
     private float dodgeCooldown = 0;
+    private float dodgeForce = 20;
 
     private bool invulnerable = false;
 
@@ -30,10 +32,10 @@ public class PlayerController : MonoBehaviour
             moveSpeed = 10;
         }
 
-        rb.linearVelocity = new Vector3(moveValue.x * moveSpeed, rb.linearVelocity.y, rb.linearVelocity.z);
+        EditXV(moveValue.x * moveSpeed);
 
         if (moveValue.y <= -0.5 && !onGround) {
-            rb.linearVelocity = new Vector3(rb.linearVelocity.x, -20, rb.linearVelocity.z);
+            AddLinearVelocity(0, fastFallSpeed, 0);
         }
     }
 
@@ -41,7 +43,7 @@ public class PlayerController : MonoBehaviour
     private void OnJump(InputValue value)
     {
         if (jumps > 0) {
-            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
+            EditYV(jumpForce);
             jumps --;
             onGround = false;
         }
@@ -57,15 +59,19 @@ public class PlayerController : MonoBehaviour
         float angle = CheckStickAngle();
         if (dodgeCooldown <= 0) {
             if (angle > 315 && angle <= 360 || angle < 45 && angle >= 0 ) {
+                EditXV(dodgeForce);
                 Debug.Log("dodge right");
             }
             if (angle > 45 && angle <= 135) {
+                EditYV(dodgeForce);
                 Debug.Log("dodge up");
             }
             if (angle > 135 && angle <= 225) {
+                EditXV(-dodgeForce);
                 Debug.Log("dodge left");
             }
             if (angle > 225 && angle <= 315) {
+                EditYV(-dodgeForce);
                 Debug.Log("dodge down");
             }
         }
@@ -78,6 +84,7 @@ public class PlayerController : MonoBehaviour
         jumps = 2;
     }
 
+    // MATHY FUNCTIONS ------------------------------------------------------------------------------------------
     public float CheckStickAngle() {
         //doom blackout coding idek but it works ig
         if (moveValue.x > 0 && moveValue.y != 0) 
@@ -97,10 +104,44 @@ public class PlayerController : MonoBehaviour
 
             Debug.Log("angle of stick 2 is " + stickAngle);
             return stickAngle;
-        } 
+        } else if ((moveValue.x == 0 && moveValue.y != 0) || (moveValue.x != 0 && moveValue.y == 0)) {
+            if (moveValue.x > 0) 
+            {
+                return 0;
+            } 
+            else if (moveValue.x < 0) 
+            {
+                return  180;
+            } 
+            else if (moveValue.y > 0) 
+            {
+                return 90;
+            } 
+            else (moveValue.y < 0) 
+            {
+                return  270;
+            }
+            Debug.Log ("wasd input detected");
+        }
         else {
             Debug.Log("hi :D");
             return stickNull;
         }
+    }
+
+    public void AddLinearVelocity(float x, float y, float z) {
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x + x, rb.linearVelocity.y + y, rb.linearVelocity.z + z);
+    }
+
+    public void MultLinearVelocity(float x, float y, float z) {
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x * x, rb.linearVelocity.y * y, rb.linearVelocity.z * z);
+    }
+
+    public void EditXV(float x) {
+        rb.linearVelocity = new Vector3(x, rb.linearVelocity.y, rb.linearVelocity.z);
+    }
+
+    public void EditYV(float y) {
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, y, rb.linearVelocity.z);
     }
 }
