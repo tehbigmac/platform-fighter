2bbbc998f6ba018ab4458b2000d8d2e8d66f049a
bigmac <tehrealbigmac@gmail.com>
2026-03-10 11:21:11 -0700

    basic camera movement and scaling

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -1,19 +1,30 @@
 using UnityEngine;
+using UnityEngine.InputSystem.XR.Haptics;
 
 public class CameraManager : MonoBehaviour
 {
 
-    public GameObject[] players = new GameObject[2]; // PLAYER COUNT GOES HERE + KB UI MANGER
+
+    private GameObject[] players = new GameObject[2]; // PLAYER COUNT GOES HERE + KB UI MANGER
 
     private Vector2 lowerBounds;
     private Vector2 upperBounds;
 
     private Vector2 cameraPos;
 
+    private float screenRatio;
+    private float playerRatio;
+    public Camera cam;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+
+
+        screenRatio = Screen.width / Screen.height;
+        Debug.Log(screenRatio);
+
         // rewrite ts later
 
         players[0] = GameObject.Find("meowl");
@@ -23,7 +34,20 @@ public class CameraManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        // Camera cam = Camera.main;
         lowerBounds = players[0].transform.position;
+        upperBounds = players[0].transform.position;
+
+        if(cam == null)
+        {
+            Debug.Log("idek");
+        }
+        
+
+        transform.localScale = new Vector3 (100f, 100f, 100f);
+
+        
 
         for (int i = 1; i < players.Length; i++) // start on second player (index 1)
         {
@@ -46,5 +70,24 @@ public class CameraManager : MonoBehaviour
         }
 
         Vector2 cameraPos = (upperBounds + lowerBounds) / 2;
+        transform.position = new Vector3(cameraPos.x, cameraPos.y, -20f);
+
+        Vector2 posDifference = upperBounds - lowerBounds;
+
+        cam.orthographicSize = posDifference.x * 2;
+
+        Debug.Log(posDifference.x * 2);
+
+
+        playerRatio = posDifference.x / posDifference.y;
+
+        
+        
+
+        if (playerRatio > screenRatio)
+        {
+            
+        }
+
     }
 }
